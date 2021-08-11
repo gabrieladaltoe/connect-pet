0dd8f35b27f79a757b3166a9362688cadd39a87e
Camila Muzzi <camilamuzzicosta@gmail.com>
2021-08-11 12:28:39 -0300

    criado a roda para tela de editar cadastro

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/indexRouter');
 var usersRouter = require('./routes/users');
+var editarPerfilRouter = require('./routes/editarPerfilRouter');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/editarPerfil', editarPerfilRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/editarPerfilRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const EditarPerfilController = require('../controllers/EditarPerfilController');
+
+router.get('/', EditarPerfilController.editarPerfil);
+
+module.exports = router;
