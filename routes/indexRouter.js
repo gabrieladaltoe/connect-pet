e9ad12b8a932ca5726cdbdab85ef57450dd072b8
Marcelo <marcelo.alves28@gmail.com>
2021-08-24 23:24:10 -0300

    Centralizado, ajuste logo o sub titulo e  botões

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,16 +1,15 @@
-var express = require('express');
-const SocialController = require('../controllers/SocialController');
+var express = require('express')
+const SocialController = require('../controllers/SocialController')
 const DesignSystemController = require('../controllers/DesignSystemController')
-var router = express.Router();
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', SocialController.index);
-router.get('/feed', SocialController.feed);
-router.get('/usuarios', SocialController.listarUsuarios);
+router.get('/', SocialController.index)
+router.get('/feed', SocialController.feed)
+router.get('/usuarios', SocialController.listarUsuarios)
 
+router.get('/design-system', DesignSystemController.viewDesignSystem)
+router.get('/profile', SocialController.profileUser)
+router.get('/users', SocialController.viewUsers)
 
-router.get('/design-system', DesignSystemController.viewDesignSystem);
-router.get('/profile', SocialController.profileUser);
-router.get('/users', SocialController.viewUsers);
-
-module.exports = router;
+module.exports = router
