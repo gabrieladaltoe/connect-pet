c9d95088af39b33acf6ffa4dda6efb23002c69e3
Camila Muzzi <camilamuzzicosta@gmail.com>
2021-10-18 21:58:07 -0300

    incluido model Publicacao

---
+++ database/models/Publicacao.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    const p = sequelize.define(
+        'Publicacao',
+        {
+            id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement:true},
+            texto: DataTypes.STRING,
+            img_pub: DataTypes.STRING,
+            usuarios_id: DataTypes.INTEGER
+        },
+        {
+            tableName: "publicacoes",
+            timestamps: false
+        }
+)
+p.associate = (models)=> {
+    p.hasMany(models.Comentario, {as:'comentarios', foreignKey:'publicacoes_id'});
+    p.belongsTo(models.Usuario, {as:'publicacao', foreignKey:'usuarios_id'});
+}
+
+
+return p;
+}
