2b6dd8ebc364b227d46a65bc036aacf77438003a
Alexandre Prado Romeu <pradoromeu@hotmail.com>
2021-11-01 22:14:58 -0300

    Alterações de layout e verifica se senhas confere

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,11 @@
 const modalLoginContent = document.querySelector('.md-login')
 const modalSubscribeContent = document.querySelector('.md-subscribe')
 
+// Btn Enviar e verifica senha
+let enviar = document.getElementById('btn_enviar')  
+let senha = document.getElementById('senha_cad')
+let senha_confirm = document.getElementById('senha_confirm_cad')
+
 const ModalLogin = {
     
     open() {
@@ -20,3 +25,11 @@ const ModalSubscribe = {
         modalSubscribeContent.classList.remove('active')
     }
 }
+
+enviar.addEventListener('click', function(event){
+    if(senha !== senha_confirm){
+        console.log('senha não confere');
+        alert('senha não confere');
+    }
+    event.preventDefault();
+})
