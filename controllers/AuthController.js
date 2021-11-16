3679c04e8198904fb41891fbecc8285dfc7ca3b0
Alexandre <pradoromeu@hotmail.com>
2021-11-16 00:08:44 -0300

    Corrigido autenticação/login/validação

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,26 +4,23 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     showLogin: (req, res) => {
-
         res.redirect('/login', {
             error: null
         });
     },
     login: async (req, res) => {
         const {email_login, senha_login } = req.body;
-        const usuario = await Usuario.findOne({where:{email_login}});
+        const usuario = await Usuario.findOne({where:{email:email_login}});
         
-        if (!usuario) {
-            return res.redirect("/login");
-        }
-      
         if(!bcrypt.compareSync(senha_login, usuario.senha) || !usuario){
-           return res.redirect("/");
+            return res.status(401).json({error:2,msg:"Usuário ou senha inválidos"});
         }
-
+       
         // gerar o token
         usuario.senha = undefined;
-        let token = jwt.sign(usuario.toJSON(), "connectpet");   
+        let token = jwt.sign(usuario.toJSON(),"connectpet");
+        req.session.usuario = JSON.stringify(usuario.id);
+        
         return res.redirect("/feed");
     },
     logout: (req,res) =>
