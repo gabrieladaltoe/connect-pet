3399d13fb7ec1cb7255d03dab32e5d2f78db69aa
Marcelo <marcelocartoes.pa@gmail.com>
2021-11-23 22:49:32 -0300

    ajsute

--- controllers/PostsController.js
+++
@@ -1,9 +0,0 @@
-const {Usuario, Perfil} = require('../database/models');
-const multer =  require('multer');
-const upload = require('../middlewares/Upload');
-
-module.exports = {
-    posts: async(req,res) => {
-        console.log(req.file);
-    }
-}
